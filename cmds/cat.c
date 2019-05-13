9331f8e14bf29f7fcdbce2b2e8497eeb03af8036
Xuefeng <i@chenxuefeng.cn>
2019-05-13 10:56:49 +0100

    ...

--- cmds/cat.c
+++ cmds/cat.c
@@ -5,7 +5,7 @@ int main(object me, string arg)
         return notify_fail("指令格式 : cat <档案名>\n      如 : cat test\n");
 
     sscanf(arg, "%s.c", arg);
-    arg = "/cmds/" + arg +".c";
+    arg = "/cmds/" + arg + ".c";
     if (file_size(arg) < 0)
     {
         return notify_fail("没有找到这个档案 T_T\n");
