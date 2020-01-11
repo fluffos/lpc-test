d10824592ac6efb911f6a5d0924e5ed48cc6eb7d
oiuv <i@oiuv.cn>
2020-01-11 21:59:50 +0800

    move efun cmds

--- cmds/efun/read_file.c
+++ cmds/efun/read_file.c
@@ -2,10 +2,10 @@
 int main(object me, string arg)
 {
     if (!arg)
-        return notify_fail("指令格式 : cat <档案名>\n      如 : cat demo/2.1.1\n");
+        return notify_fail("指令格式 : cat <档案名>\n      如 : cat /cmds/demo/2.1.1\n");
 
     sscanf(arg, "%s.c", arg);
-    arg = "/cmds/" + arg + ".c";
+    arg = arg + ".c";
     if (file_size(arg) < 0)
     {
         return notify_fail("cmds 目录下没有找到这个档案 T_T\n");
