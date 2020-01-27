61410f92da10c05ba169f93eb6c8ca53b58832ad
oiuv <i@oiuv.cn>
2020-01-27 15:36:52 +0800

    update cmds

--- cmds/efun/get_dir.c
+++ cmds/efun/get_dir.c
@@ -2,9 +2,21 @@
 
 int main(object me, string arg)
 {
-    if (arg && file_size(arg) != -1)
+    if (arg)
     {
-        print_r(get_dir(arg));
+        if (file_size(arg) == -2)
+        {
+            if (arg[strlen(arg) - 1] != '/') arg += "/";
+            print_r(get_dir(arg));
+        }
+        else if (file_size(arg) == -1)
+        {
+            return notify_fail(HIR "目录不存在或你无权读取。\n" NOR);
+        }
+        else
+        {
+            print_r(get_dir(arg, -1));
+        }
     }
     else
     {
