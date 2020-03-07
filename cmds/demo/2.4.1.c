994c71911ec6310035c46850f8fd0c19f3d5d9f7
oiuv <i@oiuv.cn>
2020-03-07 17:36:24 +0800

    add demo

---
+++ cmds/demo/2.4.1.c
@@ -0,0 +1,12 @@
+int main()
+{
+    float f = 251;
+    int w = 19.427;
+    int x = 92.78;
+    int y = 0.52;
+    int z = -87.27;
+
+    printf("f = %f, w = %d, x = %d, y = %d, z = %d\n", f, w, x, y, z);
+
+    return 1;
+}
