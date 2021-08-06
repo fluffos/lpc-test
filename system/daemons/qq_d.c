eea9e5ab18b0ce4ae1e13a747f343122cf9cdf78
oiuv <i@oiuv.cn>
2021-08-06 12:35:30 +0800

    update qq_d

--- system/daemons/qq_d.c
+++ system/daemons/qq_d.c
@@ -1,7 +1,7 @@
 /** QQ_D QQ群消息转发机器人
  *
  * 机器人服务端使用mirai开源框架，需要自己配置QQ API服务器
- * 服务器配置文档：https://project-mirai.github.io/mirai-api-http/
+ * 服务器配置文档：https://github.com/project-mirai/mirai-api-http
  *
  */
 #include <ansi.h>
@@ -11,14 +11,14 @@
 // QQ消息API服务器配置
 nosave string host = "mud.ren";
 nosave string addr = "118.190.104.241 8006";
-nosave string mirai_authKey = "QQ7300637-6427";
+nosave string mirai_verifyKey = "QQ7300637-6427";
 nosave string mirai_qq = "21791131";
 // 游戏消息转发到指定的QQ群
-nosave string group = "302388378";
+nosave string group = "285533476";
 nosave mapping status = ([]);
 nosave string session;
 
-private void verify();
+private void bind();
 private void websocket();
 
 private void http(int fd)
@@ -26,7 +26,7 @@ private void http(int fd)
     socket_write(fd, status[fd]["http"]);
 }
 
-private void receive_auth(int fd, mixed result)
+private void receive_verify(int fd, mixed result)
 {
     int n = strsrch(result, "{");
     // debug_message(result);
@@ -42,11 +42,11 @@ private void receive_auth(int fd, mixed result)
     {
         socket_close(fd);
         debug_message("QQ_D 开始认证！");
-        verify();
+        bind();
     }
 }
 
-private void receive_verify(int fd, mixed result)
+private void receive_bind(int fd, mixed result)
 {
     int n = strsrch(result, "{");
     // debug_message(result);
@@ -82,6 +82,7 @@ private void receive_data(int fd, mixed result)
         res = trim(result[n..]);
         json = json_decode(res);
         debug_message(sprintf("%O", json));
+        json = json["data"];
         sender = json["sender"];
         type = json["type"];
         // 这里只做最傻瓜的处理
@@ -156,7 +157,7 @@ private void websocket()
 {
     int fd;
     int ret;
-    string path = "/message?sessionKey=" + session;
+    string path = "/all?verifyKey=" + mirai_verifyKey + "&sessionKey=" + session;
 
     fd = socket_create(STREAM, "receive_callback", "socket_shutdown");
     status[fd] = ([]);
@@ -170,37 +171,37 @@ private void websocket()
     }
 }
 // 绑定session到QQ
-private void verify()
+private void bind()
 {
     int fd;
     int ret;
-    string path = "/verify";
+    string path = "/bind";
     string body = "{\"sessionKey\":\"" + session + "\",\"qq\":\"" + mirai_qq + "\"}";
 
     fd = socket_create(STREAM, "receive_callback", "socket_shutdown");
     status[fd] = ([]);
     status[fd]["http"] = "POST " + path + " HTTP/1.1\nHost: " + host + "\nContent-Type: application/json\nContent-Length: " + strlen(body) + "\r\n\r\n" + body;
     // debug_message(status[fd]["http"]);
-    ret = socket_connect(fd, addr, "receive_verify", "http");
+    ret = socket_connect(fd, addr, "receive_bind", "http");
     if (ret != EESUCCESS)
     {
         debug_message("激活服务器连接失败。\n");
         socket_close(fd);
     }
 }
-// 获取session
-private void auth()
+// 认证获取session
+private void verify()
 {
     int fd;
     int ret;
-    string path = "/auth";
-    string body = "{\"authKey\":\"" + mirai_authKey + "\"}";
+    string path = "/verify";
+    string body = "{\"verifyKey\":\"" + mirai_verifyKey + "\"}";
 
     fd = socket_create(STREAM, "receive_callback", "socket_shutdown");
     status[fd] = ([]);
     status[fd]["http"] = "POST " + path + " HTTP/1.1\nHost: " + host + "\nContent-Type: application/json\nContent-Length: " + strlen(body) + "\r\n\r\n" + body;
     // debug_message(status[fd]["http"]);
-    ret = socket_connect(fd, addr, "receive_auth", "http");
+    ret = socket_connect(fd, addr, "receive_verify", "http");
     if (ret != EESUCCESS)
     {
         debug_message("认证服务器连接失败。\n");
@@ -210,5 +211,5 @@ private void auth()
 
 void create()
 {
-    auth();
+    verify();
 }
